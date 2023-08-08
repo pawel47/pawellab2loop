c365a1bd2a4d115a7efdc38611e0e6f7b1d18888
pawel47 <pawelwierzbicki@rocketmail.com>
2023-08-08 20:50:56 +0000

    loop
    Signed-off-by: pawel47 <pawelwierzbicki@rocketmail.com>

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "mbed.h"
+#include <cstdio>
+
+
+
+   int main()
+   {
+    printf("lab 2 program\r\n");
+    int max_term = 10;
+    int term_1 = 0;
+    int term_2 = 1;
+    int next_term = 0;
+    for (int i=1; i<max_term; i++ ){
+        if(i == 1)
+        printf("%d, ",term_1);
+        if(i == 2) 
+        printf("%d, " ,term_2);
+        else{
+            next_term = term_1 + term_2;
+            term_1 = term_2;
+            term_2 = next_term;
+            printf("%d," ,next_term);
+        }
+    }
+    printf("\r\n");
+    while (true) {
+    
+
+    }
+
+   }
+
